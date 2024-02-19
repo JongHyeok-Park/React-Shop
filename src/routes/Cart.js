3973deff366aca793cb97165546c10ecbb6880a7
Jonghyeok <jhpmuscle@gmail.com>
2024-02-20 00:52:38 +0900

    memo, useMemo

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -2,15 +2,37 @@ import { Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { increaseCount } from "../store/cartSlice";
 import { setAge } from "./../store/userSlice";
+import { memo, useMemo, useState } from "react";
+
+function foo() {   // 매우 오래 걸리는 함수
+  let sum = 0;
+  for (let i = 0; i < 1000000000; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+let Child = memo( function() {  // props가 변경될 때만 재렌더링함
+  console.log("재랜더링");
+  return (
+    <div>
+      자식임
+    </div>
+  )
+})
 
 function Cart(props) {
   let dispatch = useDispatch();
   let products = useSelector(state => state.cart);
   let user = useSelector(state => state.user)
-
+  let [count, setCount] = useState(0);
+  useMemo(() => { return foo() })   // 컴포넌트 렌더링시 1회만 실행해줌 (useEffect랑 비슷함. but, useEffect는 html보여준 후 실행됨)
   return (
     <div>
-      {user.age}살 {user.name}의 장바구니
+      <Child count={count}></Child>
+      <button onClick={() => { setCount(count + 1) }}>+</button>
+
+      <h6>{user.age}살 {user.name}의 장바구니</h6>
       <button onClick={() => {
         dispatch(setAge(5));
       }}>버튼</button>
